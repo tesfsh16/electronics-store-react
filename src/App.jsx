175a34664231443854be62167d5ac3b14a822f04
tesfsh16 <ayenewmakbel@gmail.com>
2026-01-01 23:28:32 +0300

    tailwind and product data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-600 shadow-lg p-5 bg-white rounded-xl">
+    <div className="min-h-screen flex item-center justify-center">
+      <h1 className="text-3x1 font-bold text-accent">
         እንኳን ወደ ኤሌክትሮኒክስ መደብርህ በሰላም መጣህ!
       </h1>
     </div>
---
+++ src/data/products.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: "MacBook Pro 14",
+    category: "Laptop",
+    price: 1999,
+    image: "/laptop.png",
+    description: "Apple M2 Pro, 16GB RAM, 512GB SSD",
+  },
+  {
+    id: 2,
+    name: "iPhone 15 Pro",
+    category: "Phone",
+    price: 999,
+    image: "/phone.png",
+    description: "A17 chip, 128GB storage",
+  },
+  {
+    id: 3,
+    name: "Sony WH-1000XM5",
+    category: "Accessories",
+    price: 399,
+    image: "/headphones.png",
+    description: "Noise Cancelling Headphones",
+  },
+  {
+    id: 4,
+    name: "Dell XPS 13",
+    category: "Laptop",
+    price: 1499,
+    image: "/laptop.png",
+    description: "Intel i7, 16GB RAM",
+  },
+];
+
+export default products;
