1b30a6be9ae9ba404f15d9bc26ed4b82a3acb5a0
tesfsh16 <ayenewmakbel@gmail.com>
2026-01-01 03:30:50 +0300

    Initial commit:electronics store project started

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {}, // ይህ መስመር ተቀይሯል
+    autoprefixer: {},
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600 shadow-lg p-5 bg-white rounded-xl">
+        እንኳን ወደ ኤሌክትሮኒክስ መደብርህ በሰላም መጣህ!
+      </h1>
+    </div>
+  );
+}
+export default App;
