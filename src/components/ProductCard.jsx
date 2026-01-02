1350babbd4e888c70a4b81aca062edf5d0ef46b8
tesfsh16 <ayenewmakbel@gmail.com>
2026-01-02 05:44:52 +0300

    product card

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,26 @@
+import Home from "../pages/Home";
+
+function ProductCard({product}){
+return(
+    <div className="bg-white rounded-lg shadow hover:shadow-lg transition">
+      <img 
+      src={product.image}
+      alt={product.name}
+      className="w-full h-40 object-cover rounded-t-lg"
+       />
+  <div className="p-4">
+    <h2 className="font-semibold text-lg">
+    {product.name}
+    </h2>
+    <p className="text-sm text-gray-600">
+        {product.description}
+    </p>
+    <p className="text-accent font-bold mt-2">
+        {product.price}
+    </p>
+  </div>
+    </div>
+);
+}
+
+export default ProductCard;
