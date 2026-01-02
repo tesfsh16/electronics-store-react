e748a2fb91d65783bdd7bb43e89e535a348fc5f6
tesfsh16 <ayenewmakbel@gmail.com>
2026-01-01 23:19:58 -0300

    connect home page to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="min-h-screen flex item-center justify-center">
-      <h1 className="text-3x1 font-bold text-accent">
-        እንኳን ወደ ኤሌክትሮኒክስ መደብርህ በሰላም መጣህ!
-      </h1>
+    <div>
+      <Home />
     </div>
   );
 }
