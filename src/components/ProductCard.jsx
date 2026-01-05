a6728711a37aab9d12e9803a774483a66ec393ca
tesfsh16 <ayenewmakbel@gmail.com>
2026-01-05 21:59:50 +0300

    add addToCart button in productCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,25 +3,25 @@ import { cartContext } from "../context/CartContext";
 
 function ProductCard({product}){
   const {addToCart} = useContext(cartContext);
-return(
-    <div className="bg-white rounded-lg shadow hover:shadow-lg transition">
-      <img 
+return (
+  <div className="bg-white rounded-lg shadow hover:shadow-lg transition">
+    <img
       src={product.image}
       alt={product.name}
       className="w-full h-40 object-cover rounded-t-lg"
-       />
-  <div className="p-4">
-    <h2 className="font-semibold text-lg">
-    {product.name}
-    </h2>
-    <p className="text-sm text-gray-600">
-        {product.description}
-    </p>
-    <p className="text-accent font-bold mt-2">
-        ${product.price}
-    </p>
-  </div>
+    />
+    <div className="p-4">
+      <h2 className="font-semibold text-lg">{product.name}</h2>
+      <p className="text-sm text-gray-600">{product.description}</p>
+      <p className="text-accent font-bold mt-2">${product.price}</p>
+      <button
+        onClick={() => addToCart(product)}
+        className="mt-4 w-full bg-blue-400 text-white py-2 rounded hover:bg-blue-700"
+      >
+        Add to cart
+      </button>
     </div>
+  </div>
 );
 }
 
