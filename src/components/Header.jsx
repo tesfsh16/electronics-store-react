1dfd7cccd47439c0d9e39e1d9fb8369813e95c55
tesfsh16 <ayenewmakbel@gmail.com>
2026-01-10 21:39:46 +0300

    use reduce function aims to add quantities

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,10 @@
 import { Link} from "react-router-dom";
 import { useContext } from "react";
 import { cartContext } from "../context/CartContext";
+
+function Header (){
+const {cartItem} = useContext(cartContext);
+
+const totalItems = cartItem.reduce((total, item)=>total + item.quantity,0);
+
+}
