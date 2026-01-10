66c0a462839f753f2a956e4b49e3cde7f9304b0b
tesfsh16 <ayenewmakbel@gmail.com>
2026-01-10 21:58:04 +0300

    SPA navigation using link in header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,5 +6,20 @@ function Header (){
 const {cartItem} = useContext(cartContext);
 
 const totalItems = cartItem.reduce((total, item)=>total + item.quantity,0);
-
+return (
+  <header className="bg-primary text-black px-6 py-4 flex justify-between item-center">
+    <Link to="/" className="text-x1 font-bold hover:underline">
+      ElectroStore
+    </Link>
+    <Link to="/cart" className="relative">
+      🛒
+      {totalItems > 0 && (
+        <span className="absolute -top-2 -right-2 bg-red-500 text-xs px-2 rounded-full">
+          {totalItems}
+        </span>
+      )}
+    </Link>
+  </header>
+);
 }
+export default Header;
