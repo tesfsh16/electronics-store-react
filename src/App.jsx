35941efb736fb0be85d27f84b3c3ce3ff2fb57e1
tesfsh16 <ayenewmakbel@gmail.com>
2026-01-17 22:48:18 +0300

    Add Footer to App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import ProductDetails from "./pages/ProductDetails";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
