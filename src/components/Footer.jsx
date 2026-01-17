c527f47c7b2b6aeedf1aac2f3af0e94593c9fae1
tesfsh16 <ayenewmakbel@gmail.com>
2026-01-17 23:10:57 +0300

    Make Footer Links Clickable

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,26 @@
+import { Link } from "react-router-dom";
 function Footer(){
     return (
-      <footer className="bg-grey-900 text-gray-300 mt-12">
+      <footer className="bg-gray-900 text-gray-300 mt-12">
         <div className="max-w-7x1 mx-auto px-6 py-8 grid gap-6 md:grid-cols-3">
           <div>
             <h3 className="text-lg font-semibold text-white ">
               Tesfsh Electronics Store
             </h3>
-            <p className="text-sm mt-2">Your one-stop shop for electroniics</p>
+            <p className="text-sm mt-2">Your one-stop shop for electronics</p>
           </div>
 
           <div>
             <h4 className="text-sm font-semibold text-white mb-2">
               Quick Links
             </h4>
-            <ul className="space-y-1 text-sm ">
-              <li>Home</li>
-              <li>Cart</li>
-              <li>Products</li>
+            <ul className="space-y-1 text-sm  ">
+              <li className="hover:underline">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="hover:underline">
+                <Link to="/Cart">Cart</Link>
+              </li>
             </ul>
           </div>
           <div className="text-sm md:text-right">
